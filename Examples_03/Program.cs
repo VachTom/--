55032c5c68489b913a269c8d3d350475bd6f5629
Vachik_Tovmasyan <tovm2504@mail.ru>
2022-07-07 12:38:45 +0300

    III_II Улучшили 3 комит суммы

--- Examples_03/Program.cs
+++ Examples_03/Program.cs
@@ -1,4 +1,8 @@
-﻿int NumberA = 1563;
-int NumberB = 4566;
+﻿int NumberA = new Random().Next(1,25); // Случайное число в массиве от 1 до 25 
+Console.WriteLine (NumberA);
+
+int NumberB = new Random().Next(1,25); // Случайное число в массиве от 1 до 25;
+Console.WriteLine (NumberB);
+
 int rezult = NumberA+NumberB;
 Console.WriteLine (rezult);
